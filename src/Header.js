7accd18dab82f6419ed42568552c0fd9d4bd75cd
= <nassiromid@gmail.com>
2020-09-09 10:08:22 -0700

    Added styling for navigation links

--- src/Header.js
+++ src/Header.js
@@ -22,6 +22,31 @@ function Header(){
             <input type="text" className="header__searchInput"/>
             <SearchIcon className="header__searchIcon" />
         </div>
+
+        <div className="header__nav">
+
+            <Link to="/login" className="header__link">
+            <div className="header__option">
+                <span className="header__option1">Hello Omid</span>
+                <span className="header__option2">Sign In</span>
+            </div>
+            </Link>
+
+            <Link to="/checkout" className="header__link">
+                <div className="header__option">
+                    <span className="header__option1">Returns</span>
+                    <span className="header__option2">& Orders</span>
+                </div>
+            </Link>
+
+            <Link to="/" className="header__link">
+                <div className="header__option">
+                    <span className="header__option1">Your</span>
+                    <span className="header__option2">Prime</span>
+                </div>
+            </Link>
+
+        </div>
           
 
             {/* search box */}
