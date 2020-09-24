18ccf8f44292f22fb2aca0a6dab6372c5fc5f250
= <nassiromid@gmail.com>
2020-09-23 18:44:02 -0700

    Sort orders by date purchased - descending

--- src/Orders.js
+++ src/Orders.js
@@ -1,8 +1,32 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import { db } from './firebase';
 
 import './Orders.css';
+import { useStateValue } from './StateProvider';
 
 function Orders() {
+
+    const [{ basket, user}, dispatch] = useStateValue();
+    const [orders, setOrders] = useState([]);
+
+    // Display all orders by date - descending
+    // When new order is placed - user is redirected to orders page and shown the most recent order on top
+    useEffect( () => {
+        
+        db
+          .collection('users')
+          .doc(user?.uid)
+          .collection('orders')
+          .orderBy('created', 'desc')
+          .onSnapshot( snapshot => ( // update data for any change in db
+              setOrders(snapshot.docs.map(doc => ({
+                  id: doc.id,
+                  data: doc.data()
+              })))
+          ))
+
+    }, []); 
+
     return (
         <div className="orders">
             orders component
